7e6c14eb95303991ee78031a38adf73440eb0a67
Ashok Kumar S <akashokkumar300@gmail.com>
2021-10-29 16:18:05 -0400

    feat: Initial application flow(main menu and login)

---
+++ src/main/java/Helper.java
@@ -0,0 +1,12 @@
+public class Helper {
+
+    public static Boolean validate(String usr, String pwd) {
+        // TODO: 10/29/21  
+        return Boolean.TRUE;
+    }
+
+    public static UserType getUserType(String usr) {
+        // TODO: 10/29/21  
+        return UserType.ADMIN;
+    }
+}
