10d426a7d58357ccc32eff0a3aa89ec9b30bd11f
Ashok Kumar S <akashokkumar300@gmail.com>
2021-11-07 18:30:19 -0500

    feat: customer activities workflow

--- src/main/resources/DDL.sql
+++ src/main/resources/DDL.sql
@@ -1,7 +1,3 @@
--- noinspection SqlDialectInspectionForFile
-
--- noinspection SqlNoDataSourceInspectionForFile
-
 create table actor(
 user_name varchar2(100) primary key,
 password varchar2(200),
@@ -23,7 +19,7 @@ status VARCHAR2(1) DEFAULT 'E'
 
 create table rr_rule(
 rr_rule_code varchar2(100) primary key,
-reward varchar2(200),
+reward varchar2(200) references reward_category(id),
 num_points number,
 version number,
 status VARCHAR2(1) DEFAULT 'E'
@@ -133,7 +129,7 @@ customer_id references customer(id) on delete CASCADE,
 activity_date date DEFAULT CURRENT_DATE,
 activity_lp_map_id references activities_for_loyalty_program(activity_lp_map_id),
 customer_redeem_activity_id references customer_redeem_activity(id),
-points number(10) 
+points number(10)
 );
 
 create table customer_redeem_activity(
@@ -151,3 +147,31 @@ points number not null,
 multiplier number not null,
 lp_program_id REFERENCES loyalty_program(id) on DELETE CASCADE
 );
+
+create or replace trigger update_reward_count_and_wallet
+after insert on customer_redeem_activity
+for each row
+begin
+    update rewards_for_loyalty_program set reward_count = reward_count - 1 where reward_lp_map_id = :new.redeem_lp_map_id;
+    update wallet set points = points - :new.points;
+end;
+
+create or replace trigger calc_points
+after insert on customer_activity
+for each row
+begin
+    if :new.points is null then
+        update customer_activity set points =
+            (select nums_points
+            from re_rile where activity_category_code =
+                               (select activity_category_code
+                               from activities_for_loyalty_program
+                               where activities_for_loyalty_program.activity_lp_map_id = :new.activity_lp_map_id) and
+                                version = (select max(version)
+                                            from re_rule
+                                            where re_rule.activity_category_code =
+                                                  (select activity_category_code
+                                                   from activities_for_loyalty_program
+                                                   where activities_for_loyalty_program.activity_lp_map_id = :new.activity_lp_map_id)));
+    end if;
+end;
